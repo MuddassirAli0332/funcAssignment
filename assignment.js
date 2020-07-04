3ac50b43c5abccebcf8aa82f2be2b457c61a6add
MuddassirAli0332 <muddassirali2016@gmail.com>
2020-07-04 19:42:16 +0500

    function assignment

---
+++ assignment.js
@@ -0,0 +1,25 @@
+// console.clear();
+
+var x, y, z;
+var answerBox = document.getElementById('txtAnswer');
+var input1 = document.getElementById('txtA');
+var input2 = document.getElementById('txtB');
+var btn = document.getElementById('btnAnswer');
+btn.addEventListener('click', onclick);
+
+function onclick() {
+  x = input1.value;
+  y = input2.value;
+  
+  var ans =add(x,y)
+  answerBox.value = ans;
+  
+}
+
+
+function add(perom1, perom2) {
+  
+  var  z = parseInt(perom1) + parseInt(perom2);
+  return z;
+};
+
