192420a3af82ddb75808033d77777fb18d864da9
MuddassirAli0332 <muddassirali2016@gmail.com>
2020-07-15 17:22:55 +0500

    adjusting javascript

--- assignment.js
+++ assignment.js
@@ -2,8 +2,10 @@
 
 var x, y, z;
 var answerBox = document.getElementById('txtAnswer');
+
 var input1 = document.getElementById('txtA');
 var input2 = document.getElementById('txtB');
+
 var btn = document.getElementById('btnAnswer');
 btn.addEventListener('click', onclick);
 
@@ -18,7 +20,6 @@ function onclick() {
 
 
 function add(perom1, perom2) {
-  
   var  z = parseInt(perom1) + parseInt(perom2);
   return z;
 };
