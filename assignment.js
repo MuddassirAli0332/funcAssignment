7a8268fe104308dfac0abe4b16c201915c5e335e
MuddassirAli0332 <muddassirali2016@gmail.com>
2020-07-24 22:17:32 +0500

    funcAssignment

--- assignment.js
+++ assignment.js
@@ -1,10 +1,14 @@
 // console.clear();
 
-var x, y, z;
+var x, y, ans;
 var answerBox = document.getElementById('txtAnswer');
 
 var input1 = document.getElementById('txtA');
 var input2 = document.getElementById('txtB');
+var rbadd = document.getElementById('rbAdd');
+var rbsub = document.getElementById('rbSub');
+var rbmul = document.getElementById('rbMul');
+var rbdiv = document.getElementById('rbDiv');
 
 var btn = document.getElementById('btnAnswer');
 btn.addEventListener('click', onclick);
@@ -12,15 +16,40 @@ btn.addEventListener('click', onclick);
 function onclick() {
   x = input1.value;
   y = input2.value;
-  
-  var ans =add(x,y)
+
+  if (rbadd.checked) {
+    ans = add(x, y);
+  } else if (rbsub.checked) {
+    ans = Substract(x, y);
+  } else if (rbmul.checked) {
+    ans = Multiply(x, y);
+  } else if (rbdiv.checked) {
+    ans = divide(x, y);
+  } else {
+    alert('Please select a valid choice')
+  }
+
   answerBox.value = ans;
-  
+
 }
 
 
 function add(perom1, perom2) {
-  var  z = parseInt(perom1) + parseInt(perom2);
+  var z = parseInt(perom1) + parseInt(perom2);
   return z;
 };
 
+function Substract(param1, param2) {
+  var z = parseInt(param1) - parseInt(param2);
+  return z;
+}
+
+function Multiply(param1, param2) {
+  var z = parseInt(param1) * parseInt(param2);
+  return z;
+}
+
+function divide(param1, param2) {
+  var z = parseInt(param1) / parseInt(param2);
+  return z;
+}
